698d20c73047b802145f6b9671e63598fbe7389a
Hana Stupková <hstupkova@gmail.com>
2021-11-14 10:45:20 +0100

    nested routes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,33 @@
+import { Route, Routes, Link } from 'react-router-dom';
+
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Link to="new-user">New User</Link>
+      <Routes>
+        <Route path="new-user" element={<p>Welcome, new user!</p>} />
+      </Routes>
+    </section>
+  );
 };
 
 export default Welcome;
+
+// React Router v5 syntax:
+
+/* import { Route } from 'react-router-dom';
+
+const Welcome = () => {
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Link to="/welocme/new-user">New User</Link>
+      <Route path="/welcome/new-user">
+        <p>Welcome, new user!</p>
+      </Route>
+    </section>
+  );
+};
+
+export default Welcome; */
